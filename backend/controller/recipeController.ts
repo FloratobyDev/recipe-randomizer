826cd65c51b4f20ea2a0a80f495026cfde73ea80
Michael Mushrush <mushrushm@gmail.com>
2024-04-08 15:34:45 -0400

    [UPDATE] Backend rewritten for modern support then refactored for readability.

---
+++ backend/controller/recipeController.ts
@@ -0,0 +1,57 @@
+import { Request, Response } from "express";
+import axios from "axios";
+const controller = {
+  getRecipes: async (req: Request, res: Response) => {
+    const { ingredients } = req.query;
+    let ingredientString: string = ingredients as string; // Explicitly define as string array
+
+    if (Array.isArray(ingredients)) {
+      ingredientString = ingredients.join(",+");
+    }
+
+    let apiEndPoint = `https://api.spoonacular.com/recipes/random?apiKey=${process.env.API_KEY}&number=1`;
+
+    if (ingredientString) {
+      apiEndPoint = `https://api.spoonacular.com/recipes/findByIngredients?apiKey=${process.env.API_KEY}&ingredients=${ingredients}&number=1`;
+    }
+
+    function isEmpty(value) {
+      if (Array.isArray(value) || Object.keys(value).length > 0) {
+        return true;
+      }
+      return false;
+    }
+
+    axios
+      .get(apiEndPoint)
+      .then(async (response) => {
+        console.log("response", response.data);
+        if (isEmpty(response.data)) {
+          console.log("response.data", response.data);
+
+          const recipeData = ingredients
+            ? response.data[0]
+            : response.data.recipes[0];
+          const apiRecipeInfoEndPoint = `https://api.spoonacular.com/recipes/${recipeData.id}/information?apiKey=${process.env.API_KEY}&includeNutrition=true`;
+          axios
+            .get(apiRecipeInfoEndPoint)
+            .then((response) => {
+              return res.send({
+                recipeData,
+                recipeInformation: response.data,
+              });
+            })
+            .catch((error) => {
+              return res.send(error);
+            });
+        } else {
+          return res.status(404).send("No recipe found");
+        }
+      })
+      .catch((error) => {
+        return res.status(404).send(error);
+      });
+  },
+};
+
+export default controller;
--- backend/index.ts
+++ backend/index.ts
@@ -1,14 +1,15 @@
-const express = require("express");
-const axios = require("axios");
-const dotenv = require("dotenv");
-const _ = require("lodash");
-const path = require("path");
+import express from "express";
+import dotenv from "dotenv";
+import path from "path";
+import controller from "./controller/recipeController";
 
 const rateLimit = require("express-rate-limit");
 const app = express();
 dotenv.config();
 const PORT = process.env.PORT || 4242;
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "public")));
 
 const limiter = rateLimit({
@@ -21,51 +22,11 @@ const limiter = rateLimit({
 
 app.use(limiter);
 
-app.get("/randomize", async (req, res) => {
-  console.log("I'm in randomize");
-
-  const ingredients = req.query?.ingredients?.join(",+");
-
-  let apiEndPoint;
-
-  if (ingredients) {
-    apiEndPoint = `https://api.spoonacular.com/recipes/findByIngredients?apiKey=${process.env.API_KEY}&ingredients=${ingredients}&number=1`;
-  } else {
-    apiEndPoint = `https://api.spoonacular.com/recipes/random?apiKey=${process.env.API_KEY}&number=1`;
-  }
-  axios
-    .get(apiEndPoint)
-    .then(async (response) => {
-      if (!_.isEmpty(response.data)) {
-        const recipeData = ingredients
-          ? response.data[0]
-          : response.data.recipes[0];
-        const apiRecipeInfoEndPoint = `https://api.spoonacular.com/recipes/${recipeData.id}/information?apiKey=${process.env.API_KEY}&includeNutrition=true`;
-        axios
-          .get(apiRecipeInfoEndPoint)
-          .then((response) => {
-            return res.send({
-              recipeData,
-              recipeInformation: response.data,
-            });
-          })
-          .catch((error) => {
-            return res.send(error);
-          });
-      } else {
-        //return error no recipe found to catch
-        return res.status(404).send("No recipe found");
-      }
-    })
-    .catch((error) => {
-      return res.send(error);
-    });
-});
-
+app.get("/randomize", controller.getRecipes);
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname + "/public/index.html"));
 });
 
 app.listen(PORT, () => {
-  console.log("Server is running on http://localhost:4242");
+  console.log(`Server is running on http://localhost:${PORT}`);
 });
